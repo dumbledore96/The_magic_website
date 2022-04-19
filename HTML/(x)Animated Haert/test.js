95a0ba4bbdb29c2f128f2c6831b6e04a98068bce
aaa1056205121 <92502899+aaa1056205121@users.noreply.github.com>
2022-04-19 21:15:29 +0800

    Add files via upload

---
+++ HTML/(x)Animated Haert/test.js
@@ -0,0 +1,25 @@
+
+
+
+    docnument.addEventListener('mousemove',function(e){
+    let body = document.querySelector('body');
+    let heart = document.createElement('span');
+    let x = e.offsetX;
+    let y = e.offsetY;
+    heart.style.left = x+'px';
+    heart.style.top = y+'px';
+
+    let size = Math.random() * 80;
+    heart.style.width = 20 + size + 'px';
+    heart.style.height = 20 + size + 'px';
+
+    let transfromvalue = Math.random() * 360;
+    heart.style.transform = 'rotate(' + transfromvalue + 'deg)';
+
+    body.appendChild(heart);
+
+    setTimeout(function(){
+        heart.remove();
+    },1000)
+
+})
